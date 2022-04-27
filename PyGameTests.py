bbe5a1837da41f7842011afaa110bd72e3d2d7b8
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-27 09:33:24 -0300

    Fix: Refatoração do código.

--- PyGameTests.py
+++ PyGameTests.py
@@ -4,13 +4,7 @@ from sys import exit
 
 pygame.init()
 
-largura = 640
-altura = 480
-
-janela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('PyGameTests')
-
-# Tema
+# Mark: Tema
 
 brown = (139, 69, 19)
 black = (0, 0, 0)
@@ -21,6 +15,14 @@ green = (0, 100, 00)
 indigo = (75, 0, 130)
 red = (128, 0, 0)
 white = (255, 255, 255)
+
+# Mark: Tela
+
+dimensions = (640, 480)
+janela = pygame.display.set_mode(dimensions)
+pygame.display.set_caption('PyGameTests')
+
+# Mark: Loop Principal
 
 while True:
     for event in pygame.event.get():
