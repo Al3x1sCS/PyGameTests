0cd0a285e99319973a0cdc6819ab9bda22604b43
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-28 11:49:35 -0300

    Feat: Adicionando obstaculos. Foram adicionados obstaculos que se movem e reaparecem em posições aleatorias.
    - Se o jogador colidir com algum obstaculo
    o jogo para, mas ainda não reinicia.

--- PyGameTests.py
+++ PyGameTests.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import Utils.Colors
 from pygame import *
@@ -24,14 +26,20 @@ running = True
 y_change = 0
 x_change = 0
 gravity = 1
+obstacles = [300, 450, 600]
+obstacles_speed = 2
+active = True
 
 # Mark: Loop Principal
 # Enquanto o jogo estiver rodando
 while running:
     timer.tick(fps)
     screen.fill(background)
-    floor = pygame.draw.rect(screen, Utils.Colors.white, [0, 220, WIDTH, 5])
-    player = pygame.draw.circle(screen, Utils.Colors.darkmagenta, [player_x, player_y], 20)
+    floor = pygame.draw.rect(screen, Utils.Colors.white, [0, 210, WIDTH, 5])
+    player = pygame.draw.circle(screen, Utils.Colors.darkmagenta, [player_x, player_y], 10)
+    obstacle0 = pygame.draw.rect(screen, Utils.Colors.red, [obstacles[0], 190, 20, 20])
+    obstacle1 = pygame.draw.rect(screen, Utils.Colors.orange, [obstacles[1], 190, 20, 20])
+    obstacle2 = pygame.draw.rect(screen, Utils.Colors.yellow, [obstacles[2], 190, 20, 20])
     # PARA cada evento no jogo testa se uma tecla está sendo pressionada ou não.
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -42,13 +50,13 @@ while running:
         # Teclas A, W, S, D
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w and y_change == 0:
-                y_change = 14
+                y_change = 18
             if event.key == pygame.K_d:
                 x_change = 2
             if event.key == pygame.K_a:
                 x_change = -2
             if event.key == pygame.K_s:
-                x_change = 0
+                x_change = 3
         # SE a tecla não estiver pressionada o movimento sera nulo.
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
@@ -57,6 +65,16 @@ while running:
                 x_change = 0
             if event.key == pygame.K_s:
                 x_change = 0
+
+    for i in range(len(obstacles)):
+        if active:
+            obstacles[i] -= obstacles_speed
+            if obstacles[i] < -20:
+                obstacles[i] = random.randint(470, 570)
+                score += 1
+            if player.colliderect(obstacle0) or player.colliderect(obstacle1) or player.colliderect(obstacle2):
+                active = False
+
     # X
     # Se certifica que o jogador esta dentro da tela no eixo X
     if 0 <= player_x <= 430:
